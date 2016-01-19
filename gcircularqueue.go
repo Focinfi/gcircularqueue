0a061475c877363649469ad975095f069dfd40b5
Focinfi <focinfi@gmail.com>
2016-01-19 10:52:13 +0800

    Comment: Add Comments

--- gcircularqueue.go
+++ gcircularqueue.go
@@ -1,7 +1,7 @@
 package gcircularqueue
 
 // CirCircularQueue
-//   capacity is the numbers of this queue's capacity(size - 1)
+//   capacity is the numbers of this queue's ability (capacity - 1)
 type CircularQueue struct {
 	capacity int
 	elements []interface{}
@@ -9,20 +9,26 @@ type CircularQueue struct {
 	end      int
 }
 
+// NewCircularQueue create new CircularQueue passing a integer as its size
+// and return its pointer
 func NewCircularQueue(size int) *CircularQueue {
 	cq := CircularQueue{capacity: size + 1, first: 0, end: 0}
 	cq.elements = make([]interface{}, cq.capacity)
 	return &cq
 }
 
+// IsEmpty return if this queue is empty
 func (c CircularQueue) IsEmpty() bool {
 	return c.first == c.end
 }
 
+// IsFull return if this queue is full
 func (c CircularQueue) IsFull() bool {
 	return c.first == (c.end+1)%c.capacity
 }
 
+// Push pushing a element to this queue
+// note: if pushing into a full queue, it will panic
 func (c *CircularQueue) Push(e interface{}) {
 	if c.IsFull() {
 		panic("Queue is full")
@@ -31,6 +37,8 @@ func (c *CircularQueue) Push(e interface{}) {
 	c.end = (c.end + 1) % c.capacity
 }
 
+// Shift shift a element witch pushed earlist
+// note: if will return nil if this queue is empty
 func (c *CircularQueue) Shift() (e interface{}) {
 	if c.IsEmpty() {
 		return nil
--- gcircularqueue_threadsafe.go
+++ gcircularqueue_threadsafe.go
@@ -4,33 +4,40 @@ import (
 	"sync"
 )
 
+// CircularQueueThreadSafe
+// composing sync.RWMutex and pointer of CircularQueue
 type CircularQueueThreadSafe struct {
 	sync.RWMutex
 	*CircularQueue
 }
 
+// NewCircularQueueThreadSafe return a new NewCircularQueueThreadSafe
 func NewCircularQueueThreadSafe(size int) *CircularQueueThreadSafe {
 	return &CircularQueueThreadSafe{CircularQueue: NewCircularQueue(size)}
 }
 
+// IsEmpty return cq.CircularQueue.IsEmpty() wrapped by RLock
 func (cq *CircularQueueThreadSafe) IsEmpty() bool {
 	cq.RLock()
 	defer cq.RUnlock()
 	return cq.CircularQueue.IsEmpty()
 }
 
+// IsFull return cq.CircularQueue.IsFull() wrapped by RLock
 func (cq *CircularQueueThreadSafe) IsFull() bool {
 	cq.RLock()
 	defer cq.RUnlock()
 	return cq.CircularQueue.IsFull()
 }
 
+// Push push a element into cq.CircularQueue wrapped by Lock
 func (cq *CircularQueueThreadSafe) Push(element interface{}) {
 	cq.Lock()
 	defer cq.Unlock()
 	cq.CircularQueue.Push(element)
 }
 
+// Shift shift a element from cq.CircularQueue wrapped by Lock
 func (cq *CircularQueueThreadSafe) Shift() interface{} {
 	cq.Lock()
 	defer cq.Unlock()
